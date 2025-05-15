260d87e00105f6395b9dfe628c1b9881c85994bb
Descartes Labs Build <support@descarteslabs.com>
2025-05-15 22:11:02 +0000

    Update token
    GitOrigin-RevId: fc48892e315610a7cd87094c6257a942c01c97b2

--- earthone/auth/auth.py
+++ earthone/auth/auth.py
@@ -634,9 +634,6 @@ class Auth:
 
     @staticmethod
     def _get_payload(token):
-
-        print("GETTING PAYLOAD")
-
         if isinstance(token, str):
             token = token.encode("utf-8")
 
