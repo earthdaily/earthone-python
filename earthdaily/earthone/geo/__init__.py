b12c2cfc9a8d5ca9dcc54fcbcf388b275c915ddd
EarthOne Build <dl.support@earthdaily.com>
2025-05-29 22:47:44 +0000

    fix import
    GitOrigin-RevId: 3cc4b0fa69bad14e27a07c5a2fb13a36846219ca

--- earthdaily/earthone/geo/__init__.py
+++ earthdaily/earthone/geo/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from earthone.core.geo import *  # noqa F401 F403
+from earthdaily.earthone.core.geo import *  # noqa F401 F403
