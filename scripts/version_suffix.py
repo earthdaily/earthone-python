02a24c9700441ee622cc08e0e17dd8a1a538f6c8
EarthOne Build <dl.support@earthdaily.com>
2025-05-30 22:01:37 +0000

    Add print statement
    GitOrigin-RevId: 5848658c24615bf1630997e278eedba645c0e30d

--- scripts/version_suffix.py
+++ scripts/version_suffix.py
@@ -13,8 +13,12 @@ with open(file_path) as f:
 for i, line in enumerate(lines):
     if line.strip().startswith("__version__"):
         base_version = line.split("=")[1].strip().strip('"\'')
-        lines[i] = f'__version__ = "{base_version}{suffix}"\n'
-        break
+        print(f"{base_version = }")
+        if suffix:
+            new_version = f"{base_version}{suffix}"
+            print(f"Adding suffix. {new_version = }")
+            lines[i] = f'__version__ = "{new_version}"\n'
+            break
 
 with open(file_path, "w") as f:
     f.writelines(lines)
