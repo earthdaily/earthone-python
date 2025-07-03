a3770810dd707668e3f3cfd16f2060718d120164
Stephen C. Pope <stephen@descarteslabs.com>
2025-07-03 16:09:27 +0000

    testing

--- earthdaily/earthone/__init__.py
+++ earthdaily/earthone/__init__.py
@@ -31,11 +31,17 @@ by providing:
     * A Python client library to access these systems
 """
 
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("earthdaily-earthone")
+except PackageNotFoundError:
+    from earthdaily.earthone.core.client import __version__
+
+
 from earthdaily.earthone import auth
 from earthdaily.earthone import config
 from earthdaily.earthone import exceptions
-from earthdaily.earthone.core.client.version import __version__
-
 from earthdaily.earthone import geo
 from earthdaily.earthone import utils
 from earthdaily.earthone import catalog
